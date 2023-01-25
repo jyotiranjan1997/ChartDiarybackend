3b36e56865651366a2cb6c416b8e805c585d4245
jyotiranjan1997 <manomohanty281@gmail.com>
2023-01-25 20:50:40 +0530

    some updated

--- src/Routes/postRoute.js
+++ src/Routes/postRoute.js
@@ -6,7 +6,7 @@ const postRoute = express.Router();
 postRoute.post("/addpost",friendsMiddleware, async (req, res) => {
   const { user_id,image_url, tags, title, containt } = req.body;
   try {
-    await Post.create({ my_id: user_id, title, containt, tags });
+    await Post.create({ my_id: user_id,image_url, title, containt, tags });
     res.status(200).send({ msg: "posted Successfully ! " });
   } catch (err) {
     res.status(500).send({ msg: "failed to post !" });
@@ -16,7 +16,7 @@ postRoute.post("/addpost",friendsMiddleware, async (req, res) => {
 postRoute.get("/",friendsMiddleware, async (req, res) => {
   const { user_id } = req.body;
   try {
-    let pots = await Post.find({ my_id: user_id });
+    let posts = await Post.find({ my_id: user_id });
     res.status(200).send({ msg: "posted Successfully ! ", posts });
   } catch (err) {
     res.status(500).send({ msg: "failed to post !" });
